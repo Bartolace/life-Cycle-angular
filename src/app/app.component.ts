4bd1a726a14e52296bfb11b686afd29c994dd87a
gabriel.bartolace <gabrielll.freitas@hotmail.com>
2023-06-23 16:58:47 -0400

    onChanges and onInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Item } from './interfaces/iItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
+  listaDeCompras!: Array<Item>;
+  itemParaSerEditado!: Item;
 
-  constructor() { }
+  constructor(private listaService: ListaDeCompraService) {}
+
+  ngOnInit(): void {
+    this.listaDeCompras = this.listaService.getListaDeCompra();
+  }
+
+  editarItem(item: Item) {
+    this.itemParaSerEditado = item;
+  }
 }
